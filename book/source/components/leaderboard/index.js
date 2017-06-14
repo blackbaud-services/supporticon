fb90c7221ab29fd744db59155a84b98ffc3f9d0b
BuildKite <buildkite@everydayhero.com>
2017-06-14 05:32:47 +0000

    Updates

--- book/source/components/leaderboard/index.js
+++ book/source/components/leaderboard/index.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import numbro from 'numbro'
 import Filter from 'constructicon/filter'
 import LeaderboardWrapper from 'constructicon/leaderboard'
