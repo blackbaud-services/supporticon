7581d33f8def604c38ce2c851751e1767bc8ae87
Olivier Pichon <olivier@pichon.co>
2017-06-14 15:23:53 +1000

    Update to use external prop-types npm package

--- source/components/total-supporters/index.js
+++ source/components/total-supporters/index.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import numbro from 'numbro'
 import Icon from 'constructicon/icon'
 import Metric from 'constructicon/metric'
