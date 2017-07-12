1e7db492497924d9f4c075f01427fa0f58a7c0fa
BuildKite <buildkite@everydayhero.com>
2017-07-12 00:20:02 +0000

    Updates

--- book/source/components/single-sign-on-link/index.js
+++ book/source/components/single-sign-on-link/index.js
@@ -1,25 +1,60 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import Button from 'constructicon/button'
 import { getBaseURL } from '../../utils/client'
 
 const SingleSignOnLink = ({
   token,
   pageURL,
-  label = 'My Fundraising Page',
-  supporterCreateSessionURL = `${getBaseURL()}/api/v2/authentication/sessions`,
+  label,
+  supporterCreateSessionURL,
+  target,
   ...props
 }) => (
   token ? (
-    <form method='POST' action={supporterCreateSessionURL}>
+    <form method='POST' action={supporterCreateSessionURL} target={target}>
       <input type='hidden' name='access_token' value={token} />
       <input type='hidden' name='return_to' value={pageURL} />
       <Button {...props} type='submit'>{label}</Button>
     </form>
   ) : (
-    <Button tag='a' href={pageURL} {...props}>
+    <Button tag='a' href={pageURL} target={target} {...props}>
       {label}
     </Button>
   )
 )
+
+SingleSignOnLink.propTypes = {
+  /**
+  * The OAuth token for an authenticated user
+  */
+  token: PropTypes.string,
+
+  /**
+  * The URL of a page
+  */
+  pageURL: PropTypes.string.required,
+
+  /**
+  * Button label
+  */
+  label: PropTypes.string,
+
+  /**
+  * The target for the form or anchor
+  */
+  target: PropTypes.oneOf([
+    '_self',
+    '_blank',
+    '_parent',
+    '_top'
+  ])
+}
+
+SingleSignOnLink.defaultProps = {
+  label: 'My Fundraising Page',
+  supporterCreateSessionURL: `${getBaseURL()}/api/v2/authentication/sessions`,
+  target: '_self'
+}
 
 export default SingleSignOnLink
