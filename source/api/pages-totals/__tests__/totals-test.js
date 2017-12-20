862c9b9a167a0e87e046d1fe5c01d60454a43bd4
Rowan Hogan <rowan.hogan@everydayhero.com>
2017-12-20 14:09:46 +1000

    Add JustGiving API support for Pages Totals Add reusable dataSource method for JG API calls

--- source/api/pages-totals/__tests__/totals-test.js
+++ source/api/pages-totals/__tests__/totals-test.js
@@ -1,38 +1,63 @@
 import moxios from 'moxios'
+import { instance, updateClient } from '../../../utils/client'
 import { fetchPagesTotals } from '..'
-import { instance } from '../../../utils/client'
+import { fetchPagesTotals as fetchEDHPagesTotals } from '../everydayhero'
+import { fetchPagesTotals as fetchJGPagesTotals } from '../justgiving'
 
 describe ('Fetch Pages Totals', () => {
-  beforeEach (() => {
-    moxios.install(instance)
-  })
-
-  afterEach (() => {
-    moxios.uninstall(instance)
-  })
-
-  it ('uses the correct url to fetch totals for a campaign', (done) => {
-    fetchPagesTotals({ campaign_id: 'au-6839' })
-    moxios.wait(() => {
-      const request = moxios.requests.mostRecent()
-      expect(request.url).to.contain('https://everydayhero.com/api/v2/pages')
-      expect(request.url).to.contain('campaign_id=au-6839')
-      done()
-    })
-  })
-
-  it ('uses the correct url to fetch totals for a charity', (done) => {
-    fetchPagesTotals({ charity_id: 'au-28' })
-    moxios.wait(() => {
-      const request = moxios.requests.mostRecent()
-      expect(request.url).to.contain('https://everydayhero.com/api/v2/pages')
-      expect(request.url).to.contain('charity_id=au-28')
-      done()
-    })
-  })
-
   it ('throws if no params are passed in', () => {
     const test = () => fetchPagesTotals()
     expect(test).to.throw
+  })
+
+  describe ('Fetch EDH Pages Totals', () => {
+    beforeEach (() => {
+      moxios.install(instance)
+    })
+
+    afterEach (() => {
+      moxios.uninstall(instance)
+    })
+
+    it ('uses the correct url to fetch totals for a campaign', (done) => {
+      fetchEDHPagesTotals({ campaign_id: 'au-6839' })
+      moxios.wait(() => {
+        const request = moxios.requests.mostRecent()
+        expect(request.url).to.contain('https://everydayhero.com/api/v2/pages')
+        expect(request.url).to.contain('campaign_id=au-6839')
+        done()
+      })
+    })
+
+    it ('uses the correct url to fetch totals for a charity', (done) => {
+      fetchEDHPagesTotals({ charity_id: 'au-28' })
+      moxios.wait(() => {
+        const request = moxios.requests.mostRecent()
+        expect(request.url).to.contain('https://everydayhero.com/api/v2/pages')
+        expect(request.url).to.contain('charity_id=au-28')
+        done()
+      })
+    })
+  })
+
+  describe ('Fetch JG Pages Totals', () => {
+    beforeEach (() => {
+      updateClient({ baseURL: 'https://api.justgiving.com', headers: { 'x-api-key': 'abcd1234' } })
+      moxios.install(instance)
+    })
+
+    afterEach (() => {
+      updateClient({ baseURL: 'https://everydayhero.com' })
+      moxios.uninstall(instance)
+    })
+
+    it ('uses the correct url to fetch totals for an event', (done) => {
+      fetchJGPagesTotals({ event: 12345 })
+      moxios.wait(function () {
+        const request = moxios.requests.mostRecent()
+        expect(request.url).to.equal('https://api.justgiving.com/v1/event/12345/pages')
+        done()
+      })
+    })
   })
 })
--- source/api/pages-totals/index.js
+++ source/api/pages-totals/index.js
@@ -1,22 +1,10 @@
-import { get } from '../../utils/client'
+import { isJustGiving } from '../../utils/client'
 import { required } from '../../utils/params'
-
-export const c = {
-  ENDPOINT: 'api/v2/pages'
-}
+import { fetchPagesTotals as fetchJGPagesTotals } from './justgiving'
+import { fetchPagesTotals as fetchEDHPagesTotals } from './everydayhero'
 
 export const fetchPagesTotals = (params = required()) => {
-  const finalParams = {
-    ...params,
-    page: 1,
-    limit: 1
-  }
-
-  const mappings = {
-    type: 'type',
-    startDate: 'start_updated_at',
-    endDate: 'end_updated_at'
-  }
-
-  return get(c.ENDPOINT, finalParams, { mappings }).then((results) => results.meta.count)
+  return isJustGiving()
+    ? fetchJGPagesTotals(params)
+    : fetchEDHPagesTotals(params)
 }
