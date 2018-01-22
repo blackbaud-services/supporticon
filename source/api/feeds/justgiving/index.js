f322aba4a84b1162d11ff1d96f1884993bad8031
danhenderson <danhenderson.web@gmail.com>
2018-01-23 08:04:59 +1000

    Add donation feed api helper

---
+++ source/api/feeds/justgiving/index.js
@@ -0,0 +1,7 @@
+export const fetchDonationFeed = () => {
+  return Promise.reject(new Error('This method is not supported for JustGiving'))
+}
+
+export const deserializeDonation = () => {
+  return Promise.reject(new Error('This method is not supported for JustGiving'))
+}
