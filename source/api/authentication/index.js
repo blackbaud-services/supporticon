8216d3686ea9df2a909b7cf8f0579d07360fa665
Rowan Hogan <rowan.hogan@everydayhero.com>
2018-02-19 17:09:35 +1000

    Add JG authentication methods

--- source/api/authentication/index.js
+++ source/api/authentication/index.js
@@ -1,70 +1,32 @@
-import { post, isJustGiving } from '../../utils/client'
+import { isJustGiving } from '../../utils/client'
 import { required } from '../../utils/params'
 
-export const c = {
-  ENDPOINT: 'api/v2/authentication'
+import {
+  resetPassword as resetJGPassword,
+  signIn as loginJGAccount,
+  signUp as registerJGAccount
+} from './justgiving'
+
+import {
+  resetPassword as resetEDHPassword,
+  signIn as loginEDHUser,
+  signUp as signUpEDHUser
+} from './everydayhero'
+
+export const resetPassword = (params = required()) => {
+  return isJustGiving()
+    ? resetJGPassword(params)
+    : resetEDHPassword(params)
 }
 
-export const resetPassword = ({
-  clientId = required(),
-  email = required(),
-  reference,
-  return_to
-}) => {
-  if (isJustGiving()) return Promise.reject('This method is not supported for JustGiving')
-
-  return post(`${c.ENDPOINT}/reset_password`, {
-    client_id: clientId,
-    email,
-    reference,
-    return_to
-  })
+export const signIn = (params = required()) => {
+  return isJustGiving()
+    ? loginJGAccount(params)
+    : loginEDHUser(params)
 }
 
-export const signIn = ({
-  clientId = required(),
-  email = required(),
-  password = required(),
-  country = 'au'
-}) => {
-  if (isJustGiving()) return Promise.reject('This method is not supported for JustGiving')
-
-  return post(`${c.ENDPOINT}/sign_in`, {
-    client_id: clientId,
-    country,
-    user: {
-      email,
-      password
-    }
-  })
-  .then((data) => ({
-    userId: data.user_id,
-    token: data.token
-  }))
-}
-
-export const signUp = ({
-  clientId = required(),
-  name = required(),
-  email = required(),
-  password = required(),
-  phone = required(),
-  country = 'au'
-}) => {
-  if (isJustGiving()) return Promise.reject('This method is not supported for JustGiving')
-
-  return post(`${c.ENDPOINT}/sign_up`, {
-    client_id: clientId,
-    country,
-    user: {
-      name,
-      email,
-      password,
-      phone
-    }
-  })
-  .then((data) => ({
-    userId: data.user_id,
-    token: data.token
-  }))
+export const signUp = (params = required()) => {
+  return isJustGiving()
+    ? registerJGAccount(params)
+    : signUpEDHUser(params)
 }
