1f2506f797d9bbfa3b1b04cb3029ca5408f0637e
BuildKite <buildkite@everydayhero.com>
2018-02-19 00:02:23 +0000

    Updates

---
+++ book/source/api/campaigns/everydayhero/index.js
@@ -0,0 +1,39 @@
+import { get } from '../../../utils/client'
+import { required } from '../../../utils/params'
+
+export const c = {
+  ENDPOINT: 'api/v2/campaigns'
+}
+
+export const fetchCampaigns = (params = required()) => {
+  const mappings = {
+    charity: 'charity_ids'
+  }
+
+  const transforms = {
+    charity: (v) => Array.isArray(v) ? v.join(',') : v
+  }
+
+  return get(c.ENDPOINT, params, { mappings, transforms })
+    .then((response) => response.campaigns)
+}
+
+export const fetchCampaign = (id = required()) => (
+  get(`${c.ENDPOINT}/${id}`)
+    .then((response) => response.campaign)
+)
+
+export const fetchCampaignGroups = (id = required()) => (
+  get(`${c.ENDPOINT}/${id}/groups`)
+    .then((response) => response.campaign_groups)
+)
+
+export const deserializeCampaign = (campaign) => ({
+  name: campaign.name,
+  id: campaign.id,
+  uuid: campaign.uuid,
+  slug: campaign.slug,
+  url: campaign.url,
+  getStartedUrl: campaign.get_started_url,
+  donateUrl: campaign.donate_url
+})
---
+++ book/source/api/campaigns/justgiving/index.js
@@ -0,0 +1,36 @@
+import { get } from '../../../utils/client'
+import { getShortName, required } from '../../../utils/params'
+
+export const c = {
+  ENDPOINT: 'v1/campaigns'
+}
+
+export const fetchCampaigns = (params = required()) => (
+  get(`${c.ENDPOINT}/${getShortName(params.charity)}`)
+    .then((response) => response.campaignsDetails)
+)
+
+export const fetchCampaign = ({
+  charity = required(),
+  campaign = required()
+}) => (
+  get(`${c.ENDPOINT}/${getShortName(charity)}/${getShortName(campaign)}`)
+)
+
+export const fetchCampaignGroups = (id = required()) => (
+  Promise.reject('This method is not supported for JustGiving')
+)
+
+export const deserializeCampaign = (campaign) => {
+  const { id, campaignUrl, causeId } = campaign
+  const slug = campaignUrl.split('/')[campaignUrl.split('/').length - 1]
+
+  return {
+    name: campaign.campaignPageName,
+    id,
+    slug,
+    url: campaignUrl,
+    getStartedUrl: `https://www.justgiving.com/fundraising-page/creation/?cid=${id}&causeId=${causeId}`,
+    donateUrl: `https://link.justgiving.com/v1/campaign/donate/campaignId/${id}`
+  }
+}
