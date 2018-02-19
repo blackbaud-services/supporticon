d5799d1b03b7a81eec8edad9b2bf2d01ff7c94da
Rowan Hogan <rowan.hogan@everydayhero.com>
2018-02-19 17:09:35 +1000

    Add JG authentication methods

---
+++ source/api/authentication/justgiving/index.js
@@ -0,0 +1,61 @@
+import { get, put } from '../../../utils/client'
+import { required } from '../../../utils/params'
+
+export const resetPassword = ({
+  email = required()
+}) => (
+  get(`v1/account/${email}/requestpasswordreminder`)
+)
+
+export const signIn = ({
+  email = required(),
+  password = required()
+}) => {
+  const { btoa } = window
+  const token = btoa(`${email}:${password}`)
+
+  return get('v1/account', {}, {}, {
+    headers: {
+      'Authorization': `Basic ${token}`
+    }
+  }).then((data) => ({
+    userId: data.userId,
+    token,
+    name: [data.firstName, data.lastName].join(' '),
+    email: data.email,
+    address: data.address
+  }))
+}
+
+export const signUp = ({
+  firstName = required(),
+  lastName = required(),
+  email = required(),
+  password = required(),
+  title = required(),
+  address = required(),
+  reference,
+  cause
+}) => (
+  put('v1/account', {
+    acceptTermsAndConditions: true,
+    firstName,
+    lastName,
+    email,
+    password,
+    address,
+    reference,
+    causeId: cause
+  }).then((data) => {
+    const { btoa } = window
+    const token = btoa(`${email}:${password}`)
+
+    return {
+      userId: data.userId,
+      token,
+      name: [data.firstName, data.lastName].join(' '),
+      email: data.email,
+      address: data.address
+    }
+  })
+)
