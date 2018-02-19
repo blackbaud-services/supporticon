c6757c7adab30e1fa95d5de808c33dfda0837054
BuildKite <buildkite@everydayhero.com>
2018-02-19 23:02:39 +0000

    Updates

---
+++ book/source/api/authentication/everydayhero/index.js
@@ -0,0 +1,60 @@
+import { post } from '../../../utils/client'
+import { required } from '../../../utils/params'
+
+export const resetPassword = ({
+  clientId = required(),
+  email = required(),
+  reference,
+  return_to
+}) => (
+  post('api/v2/authentication/reset_password', {
+    client_id: clientId,
+    email,
+    reference,
+    return_to
+  })
+)
+
+export const signIn = ({
+  clientId = required(),
+  email = required(),
+  password = required(),
+  country = 'au'
+}) => (
+  post('api/v2/authentication/sign_in', {
+    client_id: clientId,
+    country,
+    user: {
+      email,
+      password
+    }
+  })
+  .then((data) => ({
+    userId: data.user_id,
+    token: data.token
+  }))
+)
+
+export const signUp = ({
+  clientId = required(),
+  name = required(),
+  email = required(),
+  password = required(),
+  phone = required(),
+  country = 'au'
+}) => (
+  post('api/v2/authentication/sign_up', {
+    client_id: clientId,
+    country,
+    user: {
+      name,
+      email,
+      password,
+      phone
+    }
+  })
+  .then((data) => ({
+    userId: data.user_id,
+    token: data.token
+  }))
+)
