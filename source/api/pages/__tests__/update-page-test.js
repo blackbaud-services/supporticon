2c34c3dbf35cfc5509ffa1236913273e10c1f40d
Rowan Hogan <rowan.hogan@everydayhero.com>
2018-02-23 11:24:00 +1000

    Create / Update JG fundraising pages

--- source/api/pages/__tests__/update-page-test.js
+++ source/api/pages/__tests__/update-page-test.js
@@ -67,11 +67,61 @@ describe ('Page | Update Page', () => {
       updateClient({ baseURL: 'https://everydayhero.com' })
     })
 
-    it ('throws method not supported error', (done) => {
-      updatePage(123, { foo: 'bar' }).catch((err) => {
-        expect(err).to.eql('This method is not supported for JustGiving')
+    it ('updates the page story correct url and data', (done) => {
+      updatePage('fundraising-page', {
+        token: '012345abcdef',
+        story: 'My updated story'
+      })
+
+      moxios.wait(() => {
+        const request = moxios.requests.mostRecent()
+        expect(request.url).to.contain('https://api.justgiving.com/v1/fundraising/pages')
+        expect(request.url).to.contain('fundraising-page')
+        expect(request.config.headers['Authorization']).to.eql('Basic 012345abcdef')
         done()
       })
+    })
+
+    it ('updates the page summary correct url and data', (done) => {
+      updatePage('fundraising-page', {
+        token: '012345abcdef',
+        summaryWhy: 'Just because giving'
+      })
+
+      moxios.wait(() => {
+        const request = moxios.requests.mostRecent()
+        expect(request.url).to.contain('https://api.justgiving.com/v1/fundraising/pages')
+        expect(request.url).to.contain('fundraising-page')
+        expect(request.config.headers['Authorization']).to.eql('Basic 012345abcdef')
+        done()
+      })
+    })
+
+    it ('updates the multiple page attributes with the correct url and data', (done) => {
+      updatePage('page-slug', {
+        token: '012345abcdef',
+        attribution: 'Jonh Smith',
+        story: 'My updated story',
+        image: 'https://image.co/photo.jpg'
+      })
+
+      moxios.wait(() => {
+        const requests = [
+          moxios.requests.at(moxios.requests.count() - 3).url,
+          moxios.requests.at(moxios.requests.count() - 2).url,
+          moxios.requests.at(moxios.requests.count() - 1).url
+        ]
+
+        expect(requests).to.include('https://api.justgiving.com/v1/fundraising/pages/page-slug')
+        expect(requests).to.include('https://api.justgiving.com/v1/fundraising/pages/page-slug/attribution')
+        expect(requests).to.include('https://api.justgiving.com/v1/fundraising/pages/page-slug/images')
+        done()
+      })
+    })
+
+    it ('throws if no token is passed', () => {
+      const test = () => updatePage('page-slug', { bogus: 'data' })
+      expect(test).to.throw
     })
   })
 })
