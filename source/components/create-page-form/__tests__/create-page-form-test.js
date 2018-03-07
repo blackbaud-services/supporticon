1fc1e124943a82d4551e6929564d0e0713564449
Rowan Hogan <rowan.hogan@everydayhero.com>
2018-03-07 16:51:14 +1000

    Fetch EDH campaign groups for create page form

--- source/components/create-page-form/__tests__/create-page-form-test.js
+++ source/components/create-page-form/__tests__/create-page-form-test.js
@@ -6,28 +6,129 @@ import CreatePageForm from '..'
 
 describe ('Components | CreatePageForm', () => {
   describe ('EDH CreatePageForm', () => {
-    it ('renders a simple create page form', () => {
-      const wrapper = mount(<CreatePageForm country='au' clientId='1234abcd' onSuccess={(res) => console.log(res)} />)
-      const inputs = wrapper.find('input')
-      const button = wrapper.find('button')
-
-      expect(inputs.length).to.eql(1)
-      expect(button.length).to.eql(1)
-      expect(button.text()).to.eql('Create Page')
+    beforeEach (() => {
+      moxios.install(instance)
     })
 
-    it ('allows a custom submit button label to be passed', () => {
-      const wrapper = mount(<CreatePageForm clientId='1234abcd' submit='Create Page on EDH' onSuccess={(res) => console.log(res)} />)
-      const button = wrapper.find('button')
-
-      expect(button.length).to.eql(1)
-      expect(button.text()).to.eql('Create Page on EDH')
+    afterEach (() => {
+      moxios.uninstall(instance)
     })
 
-    it ('allows for custom fields to be passed', () => {
-      const wrapper = mount(<CreatePageForm clientId='1234abcd' fields={{ name: { type: 'text'} }} onSuccess={(res) => console.log(res)} />)
-      const inputs = wrapper.find('input')
-      expect(inputs.length).to.eql(2)
+    it ('renders a simple create page form', (done) => {
+      const wrapper = mount(<CreatePageForm campaignId='au-123' token='1234abcd' onSuccess={(res) => console.log(res)} />)
+
+      moxios.wait(() => {
+        const request = moxios.requests.mostRecent()
+        expect(request.url).to.contain('https://everydayhero.com/api/v2/campaigns')
+        expect(request.url).to.contain('au-123')
+
+        request.respondWith({
+          status: 200,
+          response: { campaign_groups: [] }
+        })
+
+        // Re-render
+        setTimeout(() => {
+          const inputs = wrapper.find('input')
+          const button = wrapper.find('button')
+
+          expect(inputs.length).to.eql(1)
+          expect(button.length).to.eql(1)
+          expect(button.text()).to.eql('Create Page')
+          done()
+        })
+      })
+    })
+
+    it ('shows a loading spinner while fetching groups', () => {
+      const wrapper = mount(<CreatePageForm campaignId='au-123' token='1234abcd' onSuccess={(res) => console.log(res)} />)
+
+      const icon = wrapper.find('svg')
+      expect(icon.length).to.eql(1)
+    })
+
+    it ('allows a custom submit button label to be passed', (done) => {
+      const wrapper = mount(<CreatePageForm campaignId='au-123' token='1234abcd' submit='Create Page on EDH' onSuccess={(res) => console.log(res)} />)
+
+      moxios.wait(() => {
+        const request = moxios.requests.mostRecent()
+        expect(request.url).to.contain('https://everydayhero.com/api/v2/campaigns')
+        expect(request.url).to.contain('au-123')
+
+        request.respondWith({
+          status: 200,
+          response: { campaign_groups: [] }
+        })
+
+        // Re-render
+        setTimeout(() => {
+          const button = wrapper.find('button')
+
+          expect(button.length).to.eql(1)
+          expect(button.text()).to.eql('Create Page on EDH')
+          done()
+        })
+      })
+    })
+
+    it ('allows for custom fields to be passed', (done) => {
+      const wrapper = mount(<CreatePageForm campaignId='au-123' token='1234abcd' fields={{ name: { type: 'text'} }} onSuccess={(res) => console.log(res)} />)
+
+      moxios.wait(() => {
+        const request = moxios.requests.mostRecent()
+        expect(request.url).to.contain('https://everydayhero.com/api/v2/campaigns')
+        expect(request.url).to.contain('au-123')
+
+        request.respondWith({
+          status: 200,
+          response: { campaign_groups: [] }
+        })
+
+        // Re-render
+        setTimeout(() => {
+          const inputs = wrapper.find('input')
+
+          expect(inputs.length).to.eql(2)
+          done()
+        })
+      })
+    })
+
+    it ('adds required group fields when fetched', (done) => {
+      const wrapper = mount(<CreatePageForm campaignId='au-456' token='1234abcd' onSuccess={(res) => console.log(res)} />)
+
+      moxios.wait(() => {
+        const request = moxios.requests.mostRecent()
+        expect(request.url).to.contain('https://everydayhero.com/api/v2/campaigns')
+        expect(request.url).to.contain('au-456')
+
+        request.respondWith({
+          status: 200,
+          response: {
+            campaign_groups: [
+              {
+                id: 1,
+                key: 'location',
+                label: 'Selection your location',
+                values: ['Brisbane', 'Sydney', 'Melbourne', 'Adelaide', 'Hobart', 'Perth', 'Canberra', 'Darwin']
+              },
+              {
+                id: 2,
+                key: 'source',
+                label: 'How did you hear about us?',
+                values: ['Online', 'TV', 'Radio', 'Word of mouth', 'Other']
+              }
+            ]
+          }
+        }).then(() => {
+          const input = wrapper.find('input')
+          const selects = wrapper.find('select')
+
+          expect(input.length).to.eql(1)
+          expect(selects.length).to.eql(2)
+          done()
+        })
+      })
     })
   })
 
@@ -43,7 +144,7 @@ describe ('Components | CreatePageForm', () => {
     })
 
     it ('renders a simple create page form with custom submit prop', () => {
-      const wrapper = mount(<CreatePageForm onSuccess={(res) => console.log(res)} />)
+      const wrapper = mount(<CreatePageForm eventId='12345' charityId='56789' onSuccess={(res) => console.log(res)} />)
       const inputs = wrapper.find('input')
       const button = wrapper.find('button')
 
@@ -53,7 +154,7 @@ describe ('Components | CreatePageForm', () => {
     })
 
     it ('allows a custom submit button label to be passed', () => {
-      const wrapper = mount(<CreatePageForm submit='Create Page on JustGiving' onSuccess={(res) => console.log(res)} />)
+      const wrapper = mount(<CreatePageForm eventId='12345' charityId='56789' submit='Create Page on JustGiving' onSuccess={(res) => console.log(res)} />)
       const button = wrapper.find('button')
 
       expect(button.length).to.eql(1)
@@ -61,7 +162,7 @@ describe ('Components | CreatePageForm', () => {
     })
 
     it ('allows for custom fields to be passed', () => {
-      const wrapper = mount(<CreatePageForm fields={{ name: { type: 'text'} }} onSuccess={(res) => console.log(res)} />)
+      const wrapper = mount(<CreatePageForm eventId='12345' charityId='56789' fields={{ name: { type: 'text'} }} onSuccess={(res) => console.log(res)} />)
       const inputs = wrapper.find('input')
       expect(inputs.length).to.eql(3)
     })
