158449f69ad825300ba6b12804aa1c567a093f55
BuildKite <buildkite@everydayhero.com>
2018-03-08 02:57:12 +0000

    Updates

---
+++ book/source/components/create-page-form/with-groups/index.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react'
+import merge from 'lodash/merge'
+import startCase from 'lodash/startCase'
+import * as validators from 'constructicon/lib/validators'
+import { fetchCampaignGroups } from '../../../api/campaigns'
+import { isJustGiving } from '../../../utils/client'
+
+import Icon from 'constructicon/icon'
+
+const withGroups = (ComponentToWrap) => (
+  class extends Component {
+    constructor (props) {
+      super(props)
+      this.fetchGroups = this.fetchGroups.bind(this)
+
+      this.state = {
+        fields: null,
+        isFetched: isJustGiving()
+      }
+    }
+
+    componentDidMount () {
+      if (!isJustGiving()) this.fetchGroups()
+    }
+
+    fetchGroups () {
+      const { campaignId } = this.props
+
+      fetchCampaignGroups(campaignId).then((groups) => {
+        const fields = {}
+
+        if (groups.length) {
+          groups.forEach(({ key, label, values }) => {
+            const name = startCase(key)
+            const options = [
+              {
+                label: `Select a ${name}`,
+                value: '',
+                disabled: true
+              },
+              ...values.map((value) => ({
+                label: value,
+                value
+              }))
+            ]
+
+            fields[key] = {
+              label,
+              options,
+              type: 'select',
+              required: true,
+              validators: [
+                validators.required(`Please select a ${name}`)
+              ]
+            }
+          })
+        }
+
+        return this.setState({
+          fields,
+          isFetched: true
+        })
+      })
+    }
+
+    render () {
+      const { isFetched } = this.state
+
+      return isFetched ? (
+        <ComponentToWrap
+          {...this.props}
+          fields={merge(this.props.fields, this.state.fields)}
+        />
+      ) : (
+        <Icon name='loading' spin />
+      )
+    }
+  }
+)
+
+export default withGroups
