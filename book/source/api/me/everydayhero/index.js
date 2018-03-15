4db1fc6b3354a9b15b4c063c7a2d9bdf34ab9af0
BuildKite <buildkite@everydayhero.com>
2018-03-15 05:49:07 +0000

    Updates

--- book/source/api/me/everydayhero/index.js
+++ book/source/api/me/everydayhero/index.js
@@ -1,6 +1,30 @@
 import { get, put } from '../../../utils/client'
 import { required } from '../../../utils/params'
 
+export const deserializeUser = (user) => ({
+  address: {
+    streetAddress: user.street_address,
+    extendedAddress: user.extended_address,
+    locality: user.locality,
+    state: user.state,
+    postcode: user.postcode,
+    country: user.country_name
+  },
+  birthday: user.birthday,
+  country: user.country_name,
+  countryCode: user.country_code,
+  email: user.email,
+  firstName: user.first_name,
+  formattedAddress: user.formattedAddress,
+  id: user.id,
+  image: null,
+  lastName: user.last_name,
+  name: user.name,
+  pageCount: user.page_ids.length,
+  phone: user.phone,
+  uuid: user.uuid
+})
+
 export const fetchCurrentUser = ({
   token = required()
 }) => (
