b63a9716a23bea0f4f88f7a50ae7492a2fc24a2b
Rowan Hogan <rowan.hogan@everydayhero.com>
2018-03-15 15:26:34 +1000

    Add deserializeUser method

--- source/api/me/index.js
+++ source/api/me/index.js
@@ -2,14 +2,22 @@ import { isJustGiving } from '../../utils/client'
 import { required } from '../../utils/params'
 
 import {
+  deserializeUser as deserializeJGUser,
   fetchCurrentUser as fetchJGCurrentUser,
   updateCurrentUser as updateJGCurrentUser
 } from './justgiving'
 
 import {
+  deserializeUser as deserializeEDHUser,
   fetchCurrentUser as fetchEDHCurrentUser,
   updateCurrentUser as updateEDHCurrentUser
 } from './everydayhero'
+
+export const deserializeUser = (user) => (
+  isJustGiving()
+    ? deserializeJGUser(user)
+    : deserializeEDHUser(user)
+)
 
 export const fetchCurrentUser = (params = required()) => {
   return isJustGiving()
--- source/api/me/justgiving/index.js
+++ source/api/me/justgiving/index.js
@@ -1,6 +1,58 @@
 import { get, put } from '../../../utils/client'
 import { required } from '../../../utils/params'
 
+const countryCode = (country) => {
+  switch (country) {
+    case 'Australia':
+      return 'AU'
+    case 'United States':
+      return 'US'
+    case 'Ireland':
+      return 'IE'
+    case 'New Zealand':
+      return 'NZ'
+    case 'Hong Kong':
+      return 'HK'
+    case 'United Kingdom':
+      return 'GB'
+    default:
+      return 'GB'
+  }
+}
+
+const formattedAddress = ({
+  line1,
+  line2,
+  townOrCity,
+  countyOrState,
+  postcodeOrZipcode,
+  country
+}) => [line1, line2, townOrCity, countyOrState, postcodeOrZipcode, country].join(', ')
+
+export const deserializeUser = (user) => ({
+  address: {
+    streetAddress: user.address.line1,
+    extendedAddress: user.address.line2,
+    locality: user.address.townOrCity,
+    state: user.address.countyOrState,
+    postcode: user.postcodeOrZipcode,
+    country: user.address.country
+  },
+  birthday: null,
+  country: user.country,
+  countryCode: countryCode(user.country),
+  email: user.email,
+  firstName: user.firstName,
+  formattedAddress: formattedAddress(user.address),
+  id: user.accountId,
+  image: user.profileImageUrls.length ? user.profileImageUrls[0]['Value'] : null,
+  lastName: user.lastName,
+  name: [user.firstName, user.lastName].join(' '),
+  pageCount: user.activePageCount,
+  phone: null,
+  uuid: user.userId
+})
+
 export const fetchCurrentUser = ({
   token = required()
 }) => (
