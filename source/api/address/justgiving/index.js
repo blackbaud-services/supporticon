979fc033b28113877135234fa9a83d89fdde3095
danhenderson <danhenderson.web@gmail.com>
2018-06-04 16:41:08 +1000

    Add API helpers for address

---
+++ source/api/address/justgiving/index.js
@@ -0,0 +1,11 @@
+export const searchAddress = () => {
+  return Promise.reject(new Error('This method is not supported by JustGiving'))
+}
+
+export const getAddressDetails = () => {
+  return Promise.reject(new Error('This method is not supported by JustGiving'))
+}
+
+export const deserializeAddress = () => {
+  return Promise.reject(new Error('This method is not supported by JustGiving'))
+}
