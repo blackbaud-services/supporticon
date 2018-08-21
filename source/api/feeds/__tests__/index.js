27afcdd5dd8a9408620d8f7561ceffbd84474230
Rowan Hogan <rowan.hogan@everydayhero.com>
2018-08-21 14:02:03 +1000

    Add JG donation feed API methods

--- source/api/feeds/__tests__/index.js
+++ source/api/feeds/__tests__/index.js
@@ -2,16 +2,16 @@ import moxios from 'moxios'
 import { fetchDonationFeed } from '..'
 import { instance, updateClient } from '../../../utils/client'
 
-describe ('Fetch EDH Donation Feed', () => {
-  beforeEach (() => {
+describe('Fetch EDH Donation Feed', () => {
+  beforeEach(() => {
     moxios.install(instance)
   })
 
-  afterEach (() => {
+  afterEach(() => {
     moxios.uninstall(instance)
   })
 
-  it ('uses the correct url', (done) => {
+  it('uses the correct url', (done) => {
     fetchDonationFeed({ campaign_id: 'au-6839' })
       .then((response) => console.log(response.data))
     moxios.wait(() => {
@@ -24,19 +24,37 @@ describe ('Fetch EDH Donation Feed', () => {
   })
 })
 
-describe ('Fetch JG Donation Feed', () => {
-  beforeEach (() => {
+describe('Fetch JG Donation Feed', () => {
+  beforeEach(() => {
+    updateClient({ baseURL: 'https://api.justgiving.com', headers: { 'x-api-key': 'abcd1234' } })
     moxios.install(instance)
-    updateClient({ baseURL: 'https://api.justgiving.com' })
   })
 
-  afterEach (() => {
-    moxios.uninstall(instance)
+  afterEach(() => {
     updateClient({ baseURL: 'https://everydayhero.com' })
+    moxios.uninstall(instance)
   })
 
-  it ('throws unsupported error', () => {
-    const test = () => fetchDonationFeed({ campaign: 'au-123' })
-    expect(test).to.throw
+  describe('uses the correct url', () => {
+    it('for a charity', (done) => {
+      fetchDonationFeed({ charity: '12345' })
+
+      moxios.wait(() => {
+        const request = moxios.requests.mostRecent()
+        expect(request.url).to.eql('https://api.justgiving.com/v1/charity/12345/donations')
+        done()
+      })
+    })
+
+    it('for a page', (done) => {
+      fetchDonationFeed({ page: { shortName: 'test-page' } })
+
+      moxios.wait(() => {
+        const request = moxios.requests.mostRecent()
+        expect(request.url).to.contain('https://api.justgiving.com/v1/fundraising/pages/test-page/donations')
+        expect(request.url).to.contain('pageSize=150')
+        done()
+      })
+    })
   })
 })
