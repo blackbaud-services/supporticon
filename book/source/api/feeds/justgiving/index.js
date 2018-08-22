801feb18567c5f3bda127328ca847254769eeefd
BuildKite <buildkite@everydayhero.com>
2018-08-22 00:42:12 +0000

    Updates

--- book/source/api/feeds/justgiving/index.js
+++ book/source/api/feeds/justgiving/index.js
@@ -1,7 +1,24 @@
-export const fetchDonationFeed = () => {
-  return Promise.reject(new Error('This method is not supported for JustGiving'))
+import moment from 'moment'
+import { get } from '../../../utils/client'
+import { getShortName, getUID } from '../../../utils/params'
+
+export const fetchDonationFeed = ({ charity, page }) => {
+  if (!charity && !page) {
+    return Promise.reject('You must pass a charity UID or page shortName for this method')
+  }
+
+  const url = charity
+    ? `v1/charity/${getUID(charity)}/donations`
+    : `v1/fundraising/pages/${getShortName(page)}/donations?pageSize=150`
+
+  return get(url).then((response) => response.donations)
 }
 
-export const deserializeDonation = () => {
-  return Promise.reject(new Error('This method is not supported for JustGiving'))
-}
+export const deserializeDonation = (donation) => ({
+  amount: donation.donorLocalAmount || donation.amount,
+  anonymous: !donation.donorDisplayName || donation.donorDisplayName.toLowerCase().trim() === 'anonymous',
+  createdAt: moment.isMoment(moment(donation.donationDate)) && moment(donation.donationDate).toISOString(),
+  currency: donation.donorLocalCurrencyCode || donation.currencyCode,
+  message: donation.message,
+  name: donation.donorDisplayName
+})
