fb354fb3267599adf4f28cb378f2a4c81e777580
Rowan Hogan <rowan.hogan@everydayhero.com>
2018-08-22 12:31:29 +1000

    Change order of precedence for type of JG donation feed

--- source/api/feeds/justgiving/index.js
+++ source/api/feeds/justgiving/index.js
@@ -7,14 +7,14 @@ export const fetchDonationFeed = ({ charity, page }) => {
     return Promise.reject('You must pass a charity UID or page shortName for this method')
   }
 
-  const url = charity
-    ? `v1/charity/${getUID(charity)}/donations`
-    : `v1/fundraising/pages/${getShortName(page)}/donations?pageSize=150`
+  const url = page
+    ? `v1/fundraising/pages/${getShortName(page)}/donations?pageSize=150`
+    : `v1/charity/${getUID(charity)}/donations`
 
-  return get(url).then((response) => response.donations)
+  return get(url).then(response => response.donations)
 }
 
-export const deserializeDonation = (donation) => ({
+export const deserializeDonation = donation => ({
   amount: donation.donorLocalAmount || donation.amount,
   anonymous: !donation.donorDisplayName || donation.donorDisplayName.toLowerCase().trim() === 'anonymous',
   createdAt: moment.isMoment(moment(donation.donationDate)) && moment(donation.donationDate).toISOString(),
