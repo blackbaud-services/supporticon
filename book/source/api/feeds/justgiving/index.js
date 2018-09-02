7b94ac1753be7d3c5c20af3892ed150f7310cb72
BuildKite <buildkite@everydayhero.com>
2018-09-02 22:35:34 +0000

    Updates

--- book/source/api/feeds/justgiving/index.js
+++ book/source/api/feeds/justgiving/index.js
@@ -2,14 +2,18 @@ import { get } from '../../../utils/client'
 import { getShortName, getUID } from '../../../utils/params'
 import jsonDate from '../../../utils/jsonDate'
 
-export const fetchDonationFeed = ({ charity, page }) => {
-  if (!charity && !page) {
-    return Promise.reject('You must pass a charity UID or page shortName for this method')
+export const fetchDonationFeed = ({ charity, page, donationRef }) => {
+  if (!charity && !page && !donationRef) {
+    return Promise.reject(
+      'You must pass a charity UID, page shortName or donationRef for this method'
+    )
   }
 
   const url = page
     ? `v1/fundraising/pages/${getShortName(page)}/donations?pageSize=150`
-    : `v1/charity/${getUID(charity)}/donations`
+    : donationRef
+      ? `v1/donation/ref/${donationRef}`
+      : `v1/charity/${getUID(charity)}/donations`
 
   return get(url).then(response => response.donations)
 }
@@ -17,8 +21,10 @@ export const fetchDonationFeed = ({ charity, page }) => {
 export const deserializeDonation = donation => ({
   amount: donation.donorLocalAmount || donation.amount,
   anonymous: !donation.donorDisplayName || donation.donorDisplayName.toLowerCase().trim() === 'anonymous',
+  charity: donation.charityId,
   createdAt: jsonDate(donation.donationDate),
   currency: donation.donorLocalCurrencyCode || donation.currencyCode,
+  donationRef: donation.thirdPartyReference,
   message: donation.message,
   name: donation.donorDisplayName
 })
