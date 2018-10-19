08dc09dc2d6b54b35f99d0710e657e6acbdf17f2
Rowan Hogan <rowan.hogan@everydayhero.com>
2018-10-19 11:20:58 +1000

    Add fetchCharity and fetchCharities methods
    Update deserializeCharity methods to handle different API responses

--- source/api/charities/__tests__/charities-test.js
+++ source/api/charities/__tests__/charities-test.js
@@ -1,18 +1,53 @@
 import moxios from 'moxios'
 import { instance, updateClient } from '../../../utils/client'
-import { searchCharities } from '..'
+import { fetchCharities, fetchCharity, searchCharities } from '..'
 
 describe('Search charities', () => {
-  describe('in everydayhero', () => {
+  describe('in everydayhero', () => {
     beforeEach(() => moxios.install(instance))
     afterEach(() => moxios.uninstall(instance))
 
-    it ('searches charities using the provided params', done => {
+    it('fetches charities using the provided params', done => {
+      fetchCharities({ campaign: 'au-123' })
+      moxios.wait(() => {
+        const request = moxios.requests.mostRecent()
+        expect(request.url).to.contain(
+          'https://everydayhero.com/api/v2/charities'
+        )
+        expect(request.url).to.contain('campaign_ids=au-123')
+        done()
+      })
+    })
+
+    it('throws if charities are requested, but no parameters are provided', () => {
+      const test = () => fetchCharities()
+      expect(test).to.throw
+    })
+
+    it('fetches a single charity', done => {
+      fetchCharity('au-123')
+      moxios.wait(() => {
+        const request = moxios.requests.mostRecent()
+        expect(request.url).to.equal(
+          'https://everydayhero.com/api/v2/charities/au-123'
+        )
+        done()
+      })
+    })
+
+    it('throws if a charity is requested, but no charity id is supplied', () => {
+      const test = () => fetchCharity()
+      expect(test).to.throw
+    })
+
+    it('searches charities using the provided params', done => {
       searchCharities({ campaign: 'au-0', q: 'foo' })
 
       moxios.wait(() => {
         const request = moxios.requests.mostRecent()
-        expect(request.url).to.contain('https://everydayhero.com/api/v2/search/charities')
+        expect(request.url).to.contain(
+          'https://everydayhero.com/api/v2/search/charities'
+        )
         expect(request.url).to.contain('campaign_id=au-0')
         expect(request.url).to.contain('q=foo')
         done()
@@ -20,7 +55,7 @@ describe('Search charities', () => {
     })
   })
 
-  describe('in justgiving', () => {
+  describe('in justgiving', () => {
     beforeEach(() => {
       updateClient({
         baseURL: 'https://api.justgiving.com',
@@ -34,12 +69,35 @@ describe('Search charities', () => {
       return moxios.uninstall(instance)
     })
 
-    it ('searches charities', done => {
+    it('throws if charities are requested', () => {
+      const test = () => fetchCharities({ campaign: 'my-campaign' })
+      expect(test).to.throw
+    })
+
+    it('fetches a single charity', done => {
+      fetchCharity('12345')
+      moxios.wait(() => {
+        const request = moxios.requests.mostRecent()
+        expect(request.url).to.equal(
+          'https://api.justgiving.com/v1/charity/12345'
+        )
+        done()
+      })
+    })
+
+    it('throws if a charity is requested, but no charity id is supplied', () => {
+      const test = () => fetchCharity()
+      expect(test).to.throw
+    })
+
+    it('searches charities', done => {
       searchCharities({ q: 'foo' })
 
       moxios.wait(() => {
         const request = moxios.requests.mostRecent()
-        expect(request.url).to.contain('https://api.justgiving.com/v1/onesearch')
+        expect(request.url).to.contain(
+          'https://api.justgiving.com/v1/onesearch'
+        )
         expect(request.url).to.contain('i=Charity')
         expect(request.url).to.contain('q=foo')
         done()
