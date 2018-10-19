b7fd8f8902a1e5e4531c9c4b98a60b2c5573dabb
BuildKite <buildkite@everydayhero.com>
2018-10-19 01:37:58 +0000

    Updates

--- book/source/api/charities/everydayhero/index.js
+++ book/source/api/charities/everydayhero/index.js
@@ -6,16 +6,28 @@ export const c = {
   SEARCH_ENDPOINT: 'api/v2/search/charities'
 }
 
-export const searchCharities = (params = required()) => {
-  return get(c.SEARCH_ENDPOINT, params)
-    .then(response => response.charities)
+export const fetchCharities = (params = required()) => {
+  const mappings = { campaign: 'campaign_ids' }
+  const transforms = { campaign: v => (Array.isArray(v) ? v.join(',') : v) }
+
+  return get(c.ENDPOINT, params, { mappings, transforms }).then(
+    response => response.charities
+  )
 }
+
+export const fetchCharity = (id = required()) =>
+  get(`${c.ENDPOINT}/${id}`).then(response => response.charity)
+
+export const searchCharities = (params = required()) =>
+  get(c.SEARCH_ENDPOINT, params).then(response => response.charities)
 
 export const deserializeCharity = charity => ({
   active: charity.active,
   campaigns: charity.campaign_uids,
+  categories: charity.causes,
   country: charity.country_code,
   description: charity.description,
+  email: charity.public_email,
   id: charity.uid,
   logo: charity.logo_url,
   name: charity.name,
--- book/source/api/charities/justgiving/index.js
+++ book/source/api/charities/justgiving/index.js
@@ -2,8 +2,14 @@ import { get } from '../../../utils/client'
 import { required } from '../../../utils/params'
 
 export const c = {
+  ENDPOINT: 'v1/charity',
   SEARCH_ENDPOINT: 'v1/onesearch'
 }
+
+export const fetchCharities = (params = required()) =>
+  Promise.reject('This method is not supported for JustGiving')
+
+export const fetchCharity = (id = required()) => get(`${c.ENDPOINT}/${id}`)
 
 export const searchCharities = (params = required()) => {
   const finalParams = {
@@ -11,21 +17,24 @@ export const searchCharities = (params = required()) => {
     i: 'Charity'
   }
 
-  return get(c.SEARCH_ENDPOINT, finalParams)
-    .then(response => (
+  return get(c.SEARCH_ENDPOINT, finalParams).then(
+    response =>
       (response.GroupedResults &&
-      response.GroupedResults.length &&
-      response.GroupedResults[0].Results) || []
-    ))
+        response.GroupedResults.length &&
+        response.GroupedResults[0].Results) ||
+      []
+  )
 }
 
 export const deserializeCharity = charity => ({
   active: true,
-  country: charity.CountryCode,
-  description: charity.Description,
+  categories: charity.categories,
+  country: charity.countryCode || charity.CountryCode,
+  description: charity.description || charity.Description,
+  email: charity.emailAddress,
   events: charity.EventIds,
-  id: charity.Id,
-  logo: charity.Logo,
-  name: charity.Name,
-  slug: charity.Link.split('/').pop()
+  id: charity.id || charity.Id,
+  logo: charity.logoAbsoluteUrl || charity.Logo,
+  name: charity.name || charity.Name,
+  slug: charity.pageShortName || charity.Link.split('/').pop()
 })
