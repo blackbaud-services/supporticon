d652e064831f3d7ea5aeb3c58a467413a07d13fa
danhenderson <danhenderson.web@gmail.com>
2019-03-24 15:35:33 +1000

    Donation totals for v2 campaigns

--- source/api/donation-totals/justgiving/index.js
+++ source/api/donation-totals/justgiving/index.js
@@ -1,31 +1,30 @@
-import { get } from '../../../utils/client'
-import {
-  getShortName,
-  getUID,
-  required,
-  dataSource
-} from '../../../utils/params'
+import client from '../../../utils/client'
+import get from 'lodash/get'
+import { getUID, required, dataSource } from '../../../utils/params'
 import { currencyCode } from '../../../utils/currencies'
 
 export const fetchDonationTotals = (params = required()) => {
   switch (dataSource(params)) {
     case 'event':
-      return get(`v1/event/${getUID(params.event)}/leaderboard`, {
+      return client.get(`v1/event/${getUID(params.event)}/leaderboard`, {
         currency: currencyCode(params.country)
       })
     case 'charity':
       // No API method supports total funds raised for a charity
       return required()
     default:
-      return get(
-        `v1/campaigns/${getShortName(params.charity)}/${getShortName(
-          params.campaign
-        )}`
-      )
+      return client.get(`campaigns/v2/campaign/${getUID(params.campaign)}`)
   }
 }
 
 export const deserializeDonationTotals = totals => ({
-  raised: totals.totalRaised || totals.raisedAmount || 0,
-  donations: totals.numberOfDirectDonations || 0
+  raised:
+    totals.totalRaised ||
+    totals.raisedAmount ||
+    get(totals, 'donationSummary.totalAmount') ||
+    0,
+  donations:
+    totals.numberOfDirectDonations ||
+    get(totals, 'donationSummary.totalNumberOfDonations') ||
+    0
 })
