b713c18b0f4e34923fa318ac68060fe4741f2966
Rowan Hogan <rowan.hogan@everydayhero.com>
2019-04-11 13:14:21 +1000

    Optionally recursively fetch all donations in EDH feed (up to 5000)

--- source/api/feeds/everydayhero/index.js
+++ source/api/feeds/everydayhero/index.js
@@ -2,25 +2,39 @@ import { get } from '../../../utils/client'
 import compact from 'lodash/compact'
 
 export const fetchDonationFeed = options => {
-  const { includeOffline, ...params } = options
+  const { includeOffline, fetchAll, ...params } = options
 
   const finalParams = {
     ...params,
-    type: compact([ 'OnlineDonation', includeOffline && 'OfflineDonation' ])
+    type: compact(['OnlineDonation', includeOffline && 'OfflineDonation'])
   }
 
+  return fetchAll
+    ? recursivelyFetchFeed(finalParams)
+    : fetchFeed(finalParams).then(({ results }) => results)
+}
+
+const fetchFeed = params => {
   const mappings = {
     campaign: 'campaign_id',
     charity: 'charity_id',
     page: 'page_id',
     team: 'team_id',
-    type: 'type'
+    type: 'type',
+    index: 'page'
   }
 
-  return get('api/v2/search/feed', finalParams, { mappings }).then(
-    response => response.results
-  )
+  return get('api/v2/search/feed', params, { mappings })
 }
+
+const recursivelyFetchFeed = (params, feed = [], index = 1) =>
+  fetchFeed({ ...params, index }).then(({ results, meta: { pagination } }) => {
+    const updatedResults = [...feed, ...results]
+
+    return pagination.last_page || index >= 50
+      ? updatedResults
+      : recursivelyFetchFeed(params, updatedResults, index + 1)
+  })
 
 export const deserializeDonation = donation => ({
   amount: donation.amount.cents / 100,
