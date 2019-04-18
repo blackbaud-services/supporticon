bcbb5c706797221540586c8213c4ec2c460d03ae
Rowan Hogan <rowan.hogan@everydayhero.com>
2019-04-18 11:30:51 +1000

    Add support for JG fetchDonation method

--- source/api/donations/justgiving/index.js
+++ source/api/donations/justgiving/index.js
@@ -1,2 +1,24 @@
+import { get } from '../../../utils/client'
+import { required } from '../../../utils/params'
+import jsonDate from '../../../utils/jsonDate'
+
+export const fetchDonation = (id = required()) => get(`/v1/donation/${id}`)
+
 export const replyToDonation = () =>
   Promise.reject(new Error('This method is not yet supported by JustGiving'))
+
+export const deserializeDonation = donation => ({
+  amount: parseFloat(donation.donorLocalAmount || donation.amount),
+  anonymous:
+    !donation.donorDisplayName ||
+    donation.donorDisplayName.toLowerCase().trim() === 'anonymous',
+  charity: donation.charityId,
+  createdAt: jsonDate(donation.donationDate),
+  currency: donation.donorLocalCurrencyCode || donation.currencyCode,
+  donationRef: donation.donationRef,
+  event: donation.eventId,
+  message: donation.message,
+  name: donation.donorDisplayName,
+  page: donation.pageShortName,
+  status: donation.status
+})
