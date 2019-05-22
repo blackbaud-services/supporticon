b9062342257245fede981ac0dd7ea0bae5d80ffc
Rowan Hogan <rowan.hogan@everydayhero.com>
2019-05-22 10:39:16 +1000

    Add get started URL for JG campaigns

--- source/api/campaigns/justgiving/index.js
+++ source/api/campaigns/justgiving/index.js
@@ -1,4 +1,4 @@
-import { get } from '../../../utils/client'
+import { get, isStaging } from '../../../utils/client'
 import { required } from '../../../utils/params'
 
 export const fetchCampaigns = () =>
@@ -11,16 +11,21 @@ export const fetchCampaignGroups = (id = required()) =>
   Promise.reject(new Error('This method is not supported for JustGiving'))
 
 export const deserializeCampaign = campaign => {
+  const subdomain = isStaging() ? 'www.staging' : 'www'
+
   return {
     name: campaign.title,
     summary: campaign.summary,
     id: campaign.campaignGuid,
+    eventId: campaign.eventId,
     slug: campaign.shortName,
     target: campaign.targetAmount,
     raised: campaign.donationSummary.totalAmount,
     raisedOffline: campaign.donationSummary.offlineAmount,
     totalDonations: campaign.donationSummary.totalNumberOfDonations,
-    getStartedUrl: null,
+    getStartedUrl: `https://${subdomain}.justgiving.com/fundraising-page/creation?campaignGuid=${
+      campaign.campaignGuid
+    }`,
     donateUrl: null
   }
 }
