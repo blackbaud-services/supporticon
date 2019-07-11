659d4756841e0fbcfeea1d0b3f55b18576e12f84
Rowan Hogan <rowan.hogan@everydayhero.com>
2019-07-12 09:32:10 +1000

    Add deserializeMetadata methods

--- source/api/metadata/index.js
+++ source/api/metadata/index.js
@@ -2,16 +2,21 @@ import { isJustGiving } from '../../utils/client'
 import { required } from '../../utils/params'
 
 import {
+  deserializeMetadata as deserializeJGMetadata,
   fetchMetadata as fetchJGMetadata,
   createMetadata as createJGMetadata,
   updateMetadata as updateJGMetadata
 } from './justgiving'
 
 import {
+  deserializeMetadata as deserializeEDHMetadata,
   fetchMetadata as fetchEDHMetadata,
   createMetadata as createEDHMetadata,
   updateMetadata as updateEDHMetadata
 } from './everydayhero'
+
+export const deserializeMetadata = (data = required()) =>
+  isJustGiving() ? deserializeJGMetadata(data) : deserializeEDHMetadata(data)
 
 export const fetchMetadata = (params = required()) =>
   isJustGiving() ? fetchJGMetadata(params) : fetchEDHMetadata(params)
