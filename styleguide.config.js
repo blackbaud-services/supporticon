ce5a3cc625fd2094ba63b27dfe3783b676dd7845
danhenderson <danhenderson.web@gmail.com>
2019-07-18 14:11:07 +1000

    Add option to make charity search a select rather than a typeahead search

--- styleguide.config.js
+++ styleguide.config.js
@@ -89,6 +89,7 @@ module.exports = {
       components: () => [
         path.resolve(__dirname, 'source/components/address-search', 'index.js'),
         path.resolve(__dirname, 'source/components/charity-search', 'index.js'),
+        path.resolve(__dirname, 'source/components/charity-select', 'index.js'),
         path.resolve(
           __dirname,
           'source/components/create-page-form',
