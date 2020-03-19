6501ef5653b1073c29b220cb80805585b97cc3db
danhenderson <danhenderson.web@gmail.com>
2020-03-19 13:25:39 +1000

    Add JG fitness totals function

--- source/api/fitness-totals/justgiving/index.js
+++ source/api/fitness-totals/justgiving/index.js
@@ -1,11 +1,18 @@
-import { get } from '../../../utils/client'
-import { required } from '../../../utils/params'
+import * as client from '../../../utils/client'
+import { paramsSerializer, required } from '../../../utils/params'
 
-export const fetchFitnessSummary = (campaign = required(), types) => {
-  return Promise.reject(
-    new Error('This method is not supported for JustGiving')
-  )
+export const fetchFitnessSummary = (campaign = required(), types) =>
+  Promise.reject(new Error('This method is not supported for JustGiving'))
+
+export const fetchFitnessTotals = (campaign = required()) => {
+  const query = {
+    campaignGuid: campaign
+  }
+
+  return client
+    .get('/v1/fitness/campaign', query, {}, { paramsSerializer })
+    .then(result => ({
+      distance: result.totalAmount,
+      elevation: result.totalAmountElevation
+    }))
 }
-
-export const fetchFitnessTotals = (campaign = required()) =>
-  get(`/v1/fitness/campaign/${campaign}`)
