b97e68c916e5fe40463b001ea0ca672b0876f2ad
Wes Gabbard <wes.gabbard@blackbaud.com>
2020-04-27 08:25:54 -0500

    remove leaderboard endpoints from page totals
    leaderbopard endpoints only return number of pages with donations, not actual pages in campaign

--- source/api/pages-totals/justgiving/index.js
+++ source/api/pages-totals/justgiving/index.js
@@ -10,11 +10,6 @@ import { currencyCode } from '../../../utils/currencies'
 const fetchEvent = id =>
   get(`v1/event/${id}/pages`).then(response => response.totalFundraisingPages)
 
-const fetchCampaign = id =>
-  servicesAPI
-    .get(`/v1/justgiving/campaigns/${id}/leaderboard`)
-    .then(({ data }) => data.meta.totalResults)
-
 export const fetchPagesTotals = (params = required()) => {
   switch (dataSource(params)) {
     case 'event':
@@ -25,31 +20,11 @@ export const fetchPagesTotals = (params = required()) => {
       return Promise.all(eventIds.map(getUID).map(fetchEvent)).then(events =>
         events.reduce((acc, total) => acc + total, 0)
       )
-    case 'campaign':
-      const campaignIds = Array.isArray(params.campaign)
-        ? params.campaign
-        : [params.campaign]
-
-      return Promise.all(campaignIds.map(getUID).map(fetchCampaign)).then(
-        campaigns => campaigns.reduce((acc, total) => acc + total, 0)
-      )
     default:
-      return get(
-        'donationsleaderboards/v1/leaderboard',
-        {
-          ...params,
-          currencyCode: currencyCode(params.country)
-        },
-        {
-          mappings: {
-            charity: 'charityIds',
-            campaign: 'campaignGuids',
-            page: 'pageGuids',
-            excludePageIds: 'excludePageGuids',
-            limit: 'take'
-          }
-        },
-        { paramsSerializer }
-      ).then(data => data.totalResults)
+      return Promise.reject(
+        new Error(
+          'This is method currently only supports using the event parameter for JustGiving'
+        )
+      )
   }
 }
