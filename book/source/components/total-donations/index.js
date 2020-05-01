2bb416896a2a2ef242eec66f54eb9fc92b206bb1
BuildKite <buildkite@everydayhero.com>
2020-05-01 01:28:49 +0000

    Updates

--- book/source/components/total-donations/index.js
+++ book/source/components/total-donations/index.js
@@ -47,6 +47,7 @@ class TotalDonations extends Component {
       event,
       country,
       group,
+      includeOffline: !excludeOffline,
       startDate,
       endDate
     })
--- book/source/components/total-funds-raised/index.js
+++ book/source/components/total-funds-raised/index.js
@@ -47,6 +47,7 @@ class TotalFundsRaised extends Component {
       event,
       country,
       group,
+      includeOffline: !excludeOffline,
       startDate,
       endDate
     })
