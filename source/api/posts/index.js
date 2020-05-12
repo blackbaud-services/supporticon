b3a86e0d0a890b3392dfcf9c57dc9931004a1384
Rowan Hogan <rowan.hogan@gmail.com>
2020-05-12 14:31:22 +1000

    Export posts methods correctly

--- source/api/posts/index.js
+++ source/api/posts/index.js
@@ -1,8 +1,22 @@
 import { isJustGiving } from '../../utils/client'
 
-import { createPost as createEDHPost } from './everydayhero'
+import {
+  deserializePost as deserializeEDHPost,
+  fetchPosts as fetchEDHPosts,
+  createPost as createEDHPost
+} from './everydayhero'
 
-import { createPost as createJGPost } from './justgiving'
+import {
+  deserializePost as deserializeJGPost,
+  fetchPosts as fetchJGPosts,
+  createPost as createJGPost
+} from './justgiving'
+
+export const deserializePost = post =>
+  isJustGiving() ? deserializeJGPost(post) : deserializeEDHPost(post)
+
+export const fetchPosts = params =>
+  isJustGiving() ? fetchJGPosts(params) : fetchEDHPosts(params)
 
 export const createPost = params =>
   isJustGiving() ? createJGPost(params) : createEDHPost(params)
