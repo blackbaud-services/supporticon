e7c83a68f38fc536b43498f0f553b9a2c8e7d894
Rowan Hogan <rowan.hogan@gmail.com>
2020-06-11 17:02:12 +1000

    Delete JG posts

--- source/api/posts/__tests__/delete-post.js
+++ source/api/posts/__tests__/delete-post.js
@@ -1,9 +1,9 @@
 import moxios from 'moxios'
 import { instance, servicesAPI, updateClient } from '../../../utils/client'
-import { createPost } from '..'
+import { deletePost } from '..'
 
-describe('Create Post', () => {
-  describe('Create EDH Post', () => {
+describe('Delete Post', () => {
+  describe('Delete EDH Post', () => {
     beforeEach(() => {
       moxios.install(instance)
     })
@@ -12,33 +12,13 @@ describe('Create Post', () => {
       moxios.uninstall(instance)
     })
 
-    it('throws if no token is passed', () => {
-      const test = () => createPost({ bogus: 'data' })
+    it('is not supported', () => {
+      const test = () => deletePost({ id: 123, token: 'token' })
       expect(test).to.throw
-    })
-
-    it('hits the supporter api with the correct url and data', done => {
-      createPost({
-        message: 'Test message',
-        pageId: 'my-page-id',
-        token: 'test-token'
-      })
-
-      moxios.wait(() => {
-        const request = moxios.requests.mostRecent()
-        const data = JSON.parse(request.config.data)
-        const headers = request.config.headers
-
-        expect(request.url).to.contain('https://everydayhero.com/api/v2/posts')
-        expect(headers.Authorization).to.equal('Bearer test-token')
-        expect(data.caption).to.equal('Test message')
-        expect(data.page_id).to.equal('my-page-id')
-        done()
-      })
     })
   })
 
-  describe('Create JG post', () => {
+  describe('Delete JG post', () => {
     beforeEach(() => {
       updateClient({
         baseURL: 'https://api.justgiving.com',
@@ -55,21 +35,18 @@ describe('Create Post', () => {
     })
 
     it('throws if no token is passed', () => {
-      const test = () => createPost({ bogus: 'data' })
+      const test = () => deletePost({ bogus: 'data' })
       expect(test).to.throw
     })
 
     it('hits the api with the correct url and data', done => {
-      createPost({
-        message: 'Test message',
-        pageId: '1234-3210-12345',
-        userId: '3210-1234-43210',
+      deletePost({
+        id: '3210-1234-43210',
         token: 'test-token'
       })
 
       moxios.wait(() => {
         const request = moxios.requests.mostRecent()
-        const data = JSON.parse(request.config.data)
         const headers = request.config.headers
 
         expect(request.url).to.contain(
