b65520284ca2e6217453364226bc334944bcc13d
BuildKite <buildkite@everydayhero.com>
2020-09-13 22:29:29 +0000

    Updates

--- book/source/utils/html/index.js
+++ book/source/utils/html/index.js
@@ -1,5 +1,20 @@
-export const stripTags = (htmlString = '') =>
-  htmlString
-    .replace(/(<([^>]+)>)/gi, '')
-    .trim('')
-    .replace(/\s\s+/g, ' ')
+const removeTags = string =>
+  string
+    .replace(/(<([^>]+)>)/gi, ' ')
+    .replace(/ *\n\n */g, '\n\n')
+    .replace(/ \./g, '.')
+    .trim()
+
+const removeConsecutiveWhitespace = string =>
+  string.replace(/\s\s+/g, ' ').trim()
+
+const removeConsecutiveSpaces = string => string.replace(/ {2,}/g, ' ').trim()
+
+export const stripTags = (htmlString = '', preserveNewlines = false) =>
+  preserveNewlines
+    ? removeConsecutiveSpaces(
+      removeTags(
+        htmlString.replace(/(<\/[p|h1-6]>)\s*(<[p|h1-6]>)/gim, '$1\n\n$2')
+      )
+    )
+    : removeConsecutiveWhitespace(removeTags(htmlString))
