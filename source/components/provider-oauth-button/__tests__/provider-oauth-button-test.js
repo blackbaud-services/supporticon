b5f14f9ba204e52e565b423e74546402469f7ad4
Wes Gabbard <wes.gabbard@blackbaud.com>
2021-02-10 14:15:20 -0600

    remove EDH from oauth button tests

--- source/components/provider-oauth-button/__tests__/provider-oauth-button-test.js
+++ source/components/provider-oauth-button/__tests__/provider-oauth-button-test.js
@@ -5,35 +5,24 @@ import { instance, updateClient } from '../../../utils/client'
 import ProviderOauthButton from '..'
 
 describe('Components | ProviderOauthButton', () => {
-  describe('EDH ProviderOauthButton', () => {
-    it('renders a default button', () => {
-      const wrapper = mount(
-        <ProviderOauthButton
-          clientId='1234'
-          redirectUri='https://everydayhero.com'
-        />
-      )
-
-      const button = wrapper.find('button')
-      expect(button.length).to.eql(1)
-      expect(button.text()).to.eql('Login with Facebook')
+  describe('JG ProviderOauthButton', () => {
+    beforeEach(() => {
+      updateClient({
+        baseURL: 'https://api.justgiving.com',
+        headers: { 'x-api-key': 'abcd1234' }
+      })
+      moxios.install(instance)
     })
 
-    it('renders a link with no popup', () => {
+    it('does not render a button', () => {
       const wrapper = mount(
         <ProviderOauthButton
           clientId='1234'
-          redirectUri='https://google.com'
-          popup={false}
+          redirectUri='https://justgiving.com'
         />
       )
-
-      const link = wrapper.find('a')
-      expect(link.length).to.eql(1)
-      expect(link.prop('href')).to.include(
-        'https://everydayhero.com/oauth/authorize'
-      )
-      expect(link.prop('href')).to.include('google.com')
+      const a = wrapper.find('a')
+      expect(a.length).to.eql(0)
     })
 
     it('sets a different provider when passed', () => {
@@ -49,35 +38,26 @@ describe('Components | ProviderOauthButton', () => {
 
       const link = wrapper.find('a')
       expect(link.length).to.eql(1)
-      expect(link.prop('href')).to.include('force_provider=strava')
       expect(link.prop('href')).to.include('google.com')
       expect(link.text()).to.eql('Connect with Strava')
     })
-  })
 
-  describe('JG ProviderOauthButton', () => {
-    beforeEach(() => {
-      updateClient({
-        baseURL: 'https://api.justgiving.com',
-        headers: { 'x-api-key': 'abcd1234' }
-      })
-      moxios.install(instance)
-    })
-
-    afterEach(() => {
-      updateClient({ baseURL: 'https://everydayhero.com' })
-      moxios.uninstall(instance)
-    })
-
-    it('does not render a button', () => {
+    it('renders a link with no popup', () => {
       const wrapper = mount(
         <ProviderOauthButton
           clientId='1234'
-          redirectUri='https://justgiving.com'
+          redirectUri='https://google.com'
+          provider='strava'
+          popup={false}
         />
       )
-      const a = wrapper.find('a')
-      expect(a.length).to.eql(0)
+
+      const link = wrapper.find('a')
+      expect(link.length).to.eql(1)
+      expect(link.prop('href')).to.include(
+        'https://www.strava.com/oauth/authorize'
+      )
+      expect(link.prop('href')).to.include('google.com')
     })
   })
 })
