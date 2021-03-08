b16844a33e4e6e5174e0aae61133bd7a0a2113a6
Wes Gabbard <wes.gabbard@blackbaud.com>
2021-03-08 13:13:00 -0600

    resolve issue with special symbols when encoding

--- source/utils/window/index.js
+++ source/utils/window/index.js
@@ -4,8 +4,20 @@ export const getCurrentUrl = () => {
   }
 }
 
+export const getCurrentHref = () => {
+  if (typeof window !== 'undefined') {
+    return `${window.location.href}`
+  }
+}
+
 export const getIsMobile = () => {
   if (typeof window !== 'undefined') {
     return navigator.userAgent.match(/Android|iPhone|iPad|iPod/i)
+  }
+}
+
+export const isFacebookApp = () => {
+  if (typeof window !== 'undefined') {
+    return navigator.userAgent.match(/FBAV|FBAN/i)
   }
 }
