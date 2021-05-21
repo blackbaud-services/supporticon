370afb44943df6dc9c7cb97600ca47e6115ddf31
Wes Gabbard <wes.gabbard@blackbaud.com>
2021-05-21 14:28:49 -0500

    add fetch page fitness

--- source/api/fitness/index.js
+++ source/api/fitness/index.js
@@ -1,6 +1,17 @@
-import { isJustGiving, servicesAPI } from '../../utils/client'
+import { get, isJustGiving, servicesAPI } from '../../utils/client'
 import { encodeBase64String } from '../../utils/base64'
 import { required } from '../../utils/params'
+
+export const fetchPageFitness = ({
+  slug = required(),
+  limit = 100,
+  offset = 0,
+  startDate,
+  endDate
+}) => {
+  const params = { limit, offset, start: startDate, end: endDate }
+  return get(`/v1/fitness/fundraising/${slug}`, params)
+}
 
 export const disconnectFitness = ({
   pageId = required(),
