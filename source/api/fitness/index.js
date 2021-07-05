4b3ee27e19d6b0734928662136433d0e9e4e043b
Rowan Hogan <rowan.hogan@gmail.com>
2021-07-05 14:04:15 +1000

    Add connectFitness & updateFitnessSettings graphQL methods

--- source/api/fitness/index.js
+++ source/api/fitness/index.js
@@ -1,6 +1,16 @@
+import capitalize from 'lodash/capitalize'
 import { get, isJustGiving, servicesAPI } from '../../utils/client'
 import { encodeBase64String } from '../../utils/base64'
 import { required } from '../../utils/params'
+
+export const allowedActivityTypes = [
+  'hike',
+  'ride',
+  'run',
+  'swim',
+  'walk',
+  'wheelchair'
+]
 
 export const fetchPageFitness = ({
   slug = required(),
@@ -11,6 +21,30 @@ export const fetchPageFitness = ({
 }) => {
   const params = { limit, offset, start: startDate, end: endDate }
   return get(`/v1/fitness/fundraising/${slug}`, params)
+}
+
+export const connectFitness = ({
+  code = required(),
+  token = required(),
+  scope = 'read,activity:read',
+  provider = 'strava'
+}) => {
+  const query = `
+    mutation {
+      ${provider === 'fitbit' ? 'connectFitbit' : 'connectFitness'} (
+        input: {
+          code: "${code}"
+          scope: "${scope}"
+        }
+      )
+    }
+  `
+
+  const headers = { Authorization: `Bearer ${token}` }
+
+  return servicesAPI
+    .post('/v1/justgiving/graphql', { query }, { headers })
+    .then(response => response.data)
 }
 
 export const disconnectFitness = ({
@@ -27,11 +61,63 @@ export const disconnectFitness = ({
       setFitnessApplicationSettings(
         input: {
           pageId: "${encodeBase64String(`Page:FUNDRAISING:${pageId}`)}",
-          name: "${provider}",
+          name: "${capitalize(provider)}",
           subscribedActivities: []
         }
       ) {
         subscribedActivities
+      }
+    }
+  `
+
+  const headers = { Authorization: `Bearer ${token}` }
+
+  return servicesAPI
+    .post('/v1/justgiving/graphql', { query }, { headers })
+    .then(response => response.data)
+}
+
+export const updateFitnessSettings = ({
+  pageId = required(),
+  token = required(),
+  provider = 'Strava',
+  measurementSystem = 'METRIC',
+  subscribedActivities = allowedActivityTypes,
+  showDistance = true,
+  showDuration = true,
+  showElevation = true,
+  showPhotos = false,
+  showMap = false,
+  showTotaliser = true
+}) => {
+  const filteredActivityTypes = subscribedActivities
+    .filter(type => allowedActivityTypes.indexOf(type.toLowerCase()) > -1)
+    .map(type => type.toUpperCase())
+
+  const query = `
+    mutation {
+      setFitnessApplicationSettings(
+        input: {
+          pageId: "${encodeBase64String(`Page:FUNDRAISING:${pageId}`)}",
+          name: "${capitalize(provider)}",
+          measurementSystem: ${measurementSystem.toUpperCase()}
+          subscribedActivities: [${filteredActivityTypes}]
+          showDistance: ${showDistance}
+          showDuration: ${showDuration}
+          showElevation: ${showElevation}
+          showPhotos: ${showPhotos}
+          showMap: ${showMap}
+          showTotaliser: ${showTotaliser}
+        }
+      ) {
+        measurementSystem
+        subscribedActivities
+        showDistance
+        showDuration
+        showElevation
+        showPhotos
+        showMap
+        showTotaliser
       }
     }
   `
