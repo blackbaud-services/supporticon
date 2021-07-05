0e841fb515a5639b83a0e62e4ae96ca812ebb95e
Rowan Hogan <rowan.hogan@gmail.com>
2021-07-05 14:04:15 +1000

    Add connectFitness & updateFitnessSettings graphQL methods

--- source/api/fitness/__tests__/fitness-test.js
+++ source/api/fitness/__tests__/fitness-test.js
@@ -1,5 +1,5 @@
 import moxios from 'moxios'
-import { disconnectFitness } from '..'
+import { connectFitness, disconnectFitness, updateFitnessSettings } from '..'
 import { instance, servicesAPI, updateClient } from '../../../utils/client'
 
 describe('Fitness', () => {
@@ -16,6 +16,30 @@ describe('Fitness', () => {
     updateClient({ baseURL: 'https://everydayhero.com' })
     moxios.uninstall(instance)
     moxios.uninstall(servicesAPI)
+  })
+
+  describe('Connect fitness', () => {
+    it('uses the correct url with JWT', done => {
+      connectFitness({
+        code: '12345678',
+        token:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+      })
+
+      moxios.wait(() => {
+        const request = moxios.requests.mostRecent()
+        expect(request.url).to.eql(
+          'https://api.blackbaud.services/v1/justgiving/graphql'
+        )
+        expect(request.config.method).to.eql('post')
+        done()
+      })
+    })
+
+    it('throws if no token is passed', () => {
+      const test = () => connectFitness({ bogus: 'data' })
+      expect(test).to.throw
+    })
   })
 
   describe('Disconnect fitness', () => {
@@ -39,6 +63,31 @@ describe('Fitness', () => {
 
     it('throws if no token is passed', () => {
       const test = () => disconnectFitness({ bogus: 'data' })
+      expect(test).to.throw
+    })
+  })
+
+  describe('Update fitness settings', () => {
+    it('uses the correct url with JWT', done => {
+      updateFitnessSettings({
+        pageId: '1234',
+        subscribedActivities: ['walk'],
+        token:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+      })
+
+      moxios.wait(() => {
+        const request = moxios.requests.mostRecent()
+        expect(request.url).to.eql(
+          'https://api.blackbaud.services/v1/justgiving/graphql'
+        )
+        expect(request.config.method).to.eql('post')
+        done()
+      })
+    })
+
+    it('throws if no token is passed', () => {
+      const test = () => updateFitnessSettings({ bogus: 'data' })
       expect(test).to.throw
     })
   })
