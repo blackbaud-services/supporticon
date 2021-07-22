c71f07ed9997b1b7c5530c3b5cd843c157e3be2b
Rowan Hogan <rowan.hogan@gmail.com>
2021-07-22 16:48:21 +1000

    Remove EDH user api methods & tests

--- source/api/me/__tests__/update-user--test.js
+++ source/api/me/__tests__/update-user--test.js
@@ -1,112 +1,33 @@
-import moxios from 'moxios'
-import { instance, updateClient } from '../../../utils/client'
+import { instance } from '../../../utils/client'
 import { updateCurrentUser } from '..'
 
 describe('Update User', () => {
-  describe('Update EDH User', () => {
-    beforeEach(() => {
-      moxios.install(instance)
-    })
-
-    afterEach(() => {
-      moxios.uninstall(instance)
-    })
-
-    it('throws if no token is passed', () => {
-      const test = () =>
-        updateCurrentUser({
-          bogus: 'data'
-        })
-      expect(test).to.throw
-    })
-
-    it('hits the supporter api with the correct url and data', done => {
-      updateCurrentUser({
-        token: '012345abcdef',
-        birthday: '1970-01-02'
-      })
-
-      moxios.wait(() => {
-        const request = moxios.requests.mostRecent()
-        expect(request.url).to.contain(
-          'https://everydayhero.com/api/v1/me?access_token=012345abcdef'
-        )
-        done()
-      })
-    })
-
-    it('errors when an invalid token is provided', done => {
-      updateCurrentUser({
-        token: 'bogus',
-        birthday: '1970-01-02'
-      })
-
-      moxios.wait(() => {
-        const request = moxios.requests.mostRecent()
-        expect(request.url).to.contain(
-          'https://everydayhero.com/api/v1/me?access_token=bogus'
-        )
-
-        request.respondWith({ status: 401 }).then(response => {
-          expect(response.status).to.eql(401)
-          done()
-        })
-      })
-    })
-
-    it('updates the user successfully with the provided params', done => {
-      updateCurrentUser({
-        token: '012345abcdef',
-        birthday: '1970-01-02'
-      })
-
-      moxios.wait(() => {
-        const request = moxios.requests.mostRecent()
-        expect(request.url).to.contain(
-          'https://everydayhero.com/api/v1/me?access_token=012345abcdef'
-        )
-
-        request.respondWith({ status: 204 }).then(response => {
-          expect(response.status).to.eql(204)
-          done()
-        })
-      })
-    })
+  beforeEach(() => {
+    moxios.install(instance)
   })
 
-  describe('Update JG User', () => {
-    beforeEach(() => {
-      updateClient({
-        baseURL: 'https://api.justgiving.com',
-        headers: { 'x-api-key': 'abcd1234' }
-      })
-      moxios.install(instance)
+  afterEach(() => {
+    moxios.uninstall(instance)
+  })
+
+  it('throws if no token is passed', () => {
+    const test = () => updateCurrentUser({ bogus: 'data' })
+    expect(test).to.throw
+  })
+
+  it('hits the JG api with the correct url and data', done => {
+    updateCurrentUser({
+      token: 'dGVzdEBleGFtcGxlLmNvbTpmb29iYXIxMjM=',
+      uuid: '123456-abcdef',
+      email: 'test@example.com',
+      firstName: 'Don'
     })
 
-    afterEach(() => {
-      updateClient({ baseURL: 'https://everydayhero.com' })
-      moxios.uninstall(instance)
-    })
-
-    it('throws if no token is passed', () => {
-      const test = () => updateCurrentUser({ bogus: 'data' })
-      expect(test).to.throw
-    })
-
-    it('hits the JG api with the correct url and data', done => {
-      updateCurrentUser({
-        token: 'dGVzdEBleGFtcGxlLmNvbTpmb29iYXIxMjM=',
-        uuid: '123456-abcdef',
-        email: 'test@example.com',
-        firstName: 'Don'
-      })
-
-      moxios.wait(() => {
-        const request = moxios.requests.mostRecent()
-        expect(request.url).to.contain('https://api.justgiving.com/v1/account')
-        expect(request.url).to.contain('123456-abcdef')
-        done()
-      })
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent()
+      expect(request.url).to.contain('https://api.justgiving.com/v1/account')
+      expect(request.url).to.contain('123456-abcdef')
+      done()
     })
   })
 })
--- source/api/me/readme.md
+++ source/api/me/readme.md
@@ -1,13 +1,9 @@
 # Goal
 
-Helpers related to fetching and updating an authenticated user with a provided OAuth token.
-
-See [the developer docs](http://developer.everydayhero.com/users/) for more information
+Helpers related to fetching and updating an authenticated user with a provided Auth token.
 
 - [signIn](#signin)
 - [signUp](#signup)
-- [resetPassword](#resetpassword)
-
 
 ## `fetchCurrentUser`
 
@@ -19,7 +15,7 @@ Fetch current user details
 
 - `params` (Object)
 
-- `token` (String) OAuth User Token
+- `token` (String) User Token _required_
 
 **Example**
 
@@ -34,16 +30,18 @@ fetchCurrentUser({ token: 'abcdef123456' })
 
 **Purpose**
 
-Update current user birthday, phone number, and/or address
+Update current user name, email, and/or address
 
 **Params**
 
 - `params` (Object)
 
-- `token` (String) OAuth User Token
+- `token` (String) User Token _required_
+- `uuid` (String) User GUID _required_
+- `email` (String) Email address
+- `firstName` (String) First Name
+- `lastName` (String) Last Name
 - `address` (String) Formatted address
-- `birthday` (String) Date in the format: `YYYY-MM-DD`
-- `phone` (String) Phone number
 
 **Example**
 
@@ -52,6 +50,8 @@ import { updateCurrentUser } from 'supporticon/api/me'
 
 updateCurrentUser({
   token: 'abcdef123456',
-  birthday: '1970-01-01'
+  uuid: 'f440df6c-1101-4331-ac78-4fc5bc276f4e'
+  email: 'test@test.co',
+  firstName: 'Test'
 })
 ```
