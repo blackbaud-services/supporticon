1e799ee88d21419ba946fa80963deee39a5d9c9c
Rowan Hogan <rowan.hogan@gmail.com>
2021-07-23 15:55:13 +1000

    Remove EDH references from LoginForm component

--- source/components/login-form/Readme.md
+++ source/components/login-form/Readme.md
@@ -2,7 +2,6 @@
 
 ```
 <LoginForm
-  clientId='26de0618f8469b55bee0e134c56e938da64566234dd935855daec36c294a5a65'
   onSuccess={(result) => alert(JSON.stringify(result))}
 />
 ```
--- source/components/login-form/__tests__/login-form-test.js
+++ source/components/login-form/__tests__/login-form-test.js
@@ -1,51 +1,29 @@
 import React from 'react'
-import moxios from 'moxios'
-import { instance, updateClient } from '../../../utils/client'
-
 import LoginForm from '..'
 
-describe('Components | LoginForm', () => {
-  describe('EDH LoginForm', () => {
-    it('renders a simple login form', () => {
-      const wrapper = mount(
-        <LoginForm clientId='1234abcd' onSuccess={res => console.log(res)} />
-      )
-      const inputs = wrapper.find('input')
-      const button = wrapper.find('button[type="submit"]')
+import { instance } from '../../../utils/client'
 
-      expect(inputs.length).to.eql(2)
-      expect(button.length).to.eql(1)
-      expect(button.text()).to.eql('Log in')
-    })
+describe('Components | LoginForm', () => {
+  beforeEach(() => {
+    moxios.install(instance)
   })
 
-  describe('JG LoginForm', () => {
-    beforeEach(() => {
-      updateClient({
-        baseURL: 'https://api.justgiving.com',
-        headers: { 'x-api-key': 'abcd1234' }
-      })
-      moxios.install(instance)
-    })
+  afterEach(() => {
+    moxios.uninstall(instance)
+  })
 
-    afterEach(() => {
-      updateClient({ baseURL: 'https://everydayhero.com' })
-      moxios.uninstall(instance)
-    })
+  it('renders a simple login form with custom submit prop', () => {
+    const wrapper = mount(
+      <LoginForm
+        submit='Sign in to JustGiving'
+        onSuccess={res => console.log(res)}
+      />
+    )
+    const inputs = wrapper.find('input')
+    const button = wrapper.find('button[type="submit"]')
 
-    it('renders a simple login form with custom submit prop', () => {
-      const wrapper = mount(
-        <LoginForm
-          submit='Sign in to JustGiving'
-          onSuccess={res => console.log(res)}
-        />
-      )
-      const inputs = wrapper.find('input')
-      const button = wrapper.find('button[type="submit"]')
-
-      expect(inputs.length).to.eql(2)
-      expect(button.length).to.eql(1)
-      expect(button.text()).to.eql('Sign in to JustGiving')
-    })
+    expect(inputs.length).to.eql(2)
+    expect(button.length).to.eql(1)
+    expect(button.text()).to.eql('Sign in to JustGiving')
   })
 })
