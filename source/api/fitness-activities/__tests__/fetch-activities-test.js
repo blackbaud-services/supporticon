9c3f02f2ca5961aeb959452e0676b89dd8e7ec99
Wes Gabbard <wes.gabbard@blackbaud.com>
2021-09-02 13:44:34 -0500

    remove campaign and team fitness activity fetch
    v1 fitness no longer returns activities. GraphQL for page types team and campaign return empty array in timeline

--- source/api/fitness-activities/__tests__/fetch-activities-test.js
+++ source/api/fitness-activities/__tests__/fetch-activities-test.js
@@ -1,13 +1,13 @@
-import { instance } from '../../../utils/client'
+import { servicesAPI } from '../../../utils/client'
 import { fetchFitnessActivities } from '..'
 
 describe('Fetch Fitness Activities', () => {
   beforeEach(() => {
-    moxios.install(instance)
+    moxios.install(servicesAPI)
   })
 
   afterEach(() => {
-    moxios.uninstall(instance)
+    moxios.uninstall(servicesAPI)
   })
 
   it('throws with incorrect params', () => {
@@ -15,33 +15,25 @@ describe('Fetch Fitness Activities', () => {
     expect(test).to.throw
   })
 
-  it('fetches activities for a campaign', done => {
-    fetchFitnessActivities({ campaign: '12345678' })
+  it('throws with fetching activities for a campaign', () => {
+    const test = () => fetchFitnessActivities({ campaign: '12345678' })
+    expect(test).to.throw
+  })
 
-    moxios.wait(() => {
-      const request = moxios.requests.mostRecent()
-      expect(request.url).to.contain('/v1/fitness/campaign')
-      expect(request.url).to.contain('campaignGuid=12345678')
-      done()
-    })
+  it('throws with fetching activities for a team', () => {
+    const test = () => fetchFitnessActivities({ team: 'test-team' })
+    expect(test).to.throw
   })
 
   it('fetches activities for a page', done => {
-    fetchFitnessActivities({ page: 'test-page', useLegacy: true })
+    fetchFitnessActivities({ page: 'test-page' })
 
     moxios.wait(() => {
       const request = moxios.requests.mostRecent()
-      expect(request.url).to.contain('/v1/fitness/fundraising/test-page')
-      done()
-    })
-  })
-
-  it('fetches activities for a team', done => {
-    fetchFitnessActivities({ team: 'test-team' })
-
-    moxios.wait(() => {
-      const request = moxios.requests.mostRecent()
-      expect(request.url).to.contain('/v1/fitness/teams/test-team')
+      console.log(request)
+      expect(request.url).to.contain(
+        'https://api.blackbaud.services/v1/justgiving/graphql'
+      )
       done()
     })
   })
