6f1bab0a54617c487af901300ee33bb808ffc07c
Rowan Hogan <rowan.hogan@gmail.com>
2021-11-01 13:12:32 +1000

    Refactor fitness leaderboard to use react-query

--- source/hooks/use-fitness-leaderboard/index.js
+++ source/hooks/use-fitness-leaderboard/index.js
@@ -1,36 +1,22 @@
-import { useState, useEffect } from 'react'
+import { useQuery } from 'react-query'
 import {
   fetchFitnessLeaderboard,
   deserializeFitnessLeaderboard
 } from '../../api/fitness-leaderboard'
 
-const useFitnessLeaderboard = params => {
-  if (useState === undefined) {
-    console.error(
-      'Current version of React does not support Hooks. Upgrade React to 16.8 to use supporticon/hooks'
-    )
-    return []
-  }
+export const useFitnessLeaderboard = (params, options) => {
+  const { deserializeMethod, refetchInterval } = options
 
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState()
-  const [leaderboard, setLeaderboard] = useState([])
-
-  useEffect(() => {
-    fetchFitnessLeaderboard(params)
-      .then(result => result.map(deserializeFitnessLeaderboard))
-      .then(deserialized => {
-        setLeaderboard(deserialized)
-        setLoading(false)
-      })
-      .catch(error => {
-        setLoading(false)
-        setError(error)
-        Promise.reject(error)
-      })
-  }, [])
-
-  return [leaderboard, loading, error]
+  return useQuery(
+    ['fitnessLeaderboard', params],
+    () =>
+      fetchFitnessLeaderboard(params)
+        .then(results => results.map(deserializeMethod || deserializeFitnessLeaderboard)),
+    {
+      placeholderData: [],
+      refetchInterval
+    }
+  )
 }
 
 export default useFitnessLeaderboard
