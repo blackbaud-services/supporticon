f453a97ea6f0a84b3556a0587932a02eefa111d4
Rowan Hogan <rowan.hogan@gmail.com>
2021-11-01 11:36:22 +1000

    Add query provider and include in docs

---
+++ source/utils/data/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+export const queryClient = new QueryClient()
+
+export const DataProvider = ({ children }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
+  )
+}
+
+export default DataProvider
