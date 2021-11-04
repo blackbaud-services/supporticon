80e52ebb4bee91c5324ab4bc3f4ed762ca945fbd
Rowan Hogan <rowan.hogan@gmail.com>
2021-11-04 16:55:55 +1000

    Refactor hooks components

--- source/hooks/use-event-info/index.js
+++ source/hooks/use-event-info/index.js
@@ -1,40 +1,11 @@
-import { useState, useEffect, useCallback } from 'react'
+import { useQuery } from 'react-query'
 import { deserializeEvent, fetchEvent } from '../../api/events'
 
-export const useEventInfo = id => {
-  if (useState === undefined) {
-    console.error(
-      'Current version of React does not support Hooks. Upgrade React to 16.8 to use supporticon/hooks'
-    )
-    return []
-  }
-
-  const [status, setStatus] = useState(null)
-  const [event, setEvent] = useState(null)
-  const [error, setError] = useState(null)
-  const reload = useCallback(async () => {
-    setStatus('fetching')
-    try {
-      const response = await fetchEvent(id)
-      if (response && response.id) {
-        setStatus('fetched')
-        setEvent(deserializeEvent(response))
-      } else {
-        setStatus('failed')
-        setError(response.message)
-      }
-    } catch (error) {
-      setStatus('failed')
-      setError(error)
-    }
-  }, [])
-
-  useEffect(
-    () => {
-      reload()
-    },
-    [reload]
+export const useEventInfo = (id, options = {}) =>
+  useQuery(
+    ['eventInfo', id],
+    () => fetchEvent(id).then(deserializeEvent),
+    options
   )
 
-  return { event, error, status, reload }
-}
+export default useEventInfo
