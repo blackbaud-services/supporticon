7211cda97a1109b762774adde412e87fe0bb6839
danhenderson <danhenderson.web@gmail.com>
2021-12-20 14:15:36 +1000

    Use new JG purple

--- source/api/qrcodes/index.js
+++ source/api/qrcodes/index.js
@@ -3,7 +3,7 @@ import { required } from '../../utils/params'
 
 export const createQrCode = (
   linkUrl = required(),
-  logoUrl = 'https://images.justgiving.com/image/f468c25c-93e5-4e2d-bcb9-eb525559fb43.jpg'
+  logoUrl = 'https://images.justgiving.com/image/justgiving-logo.png'
 ) => {
   return get('/v1/qrcodes/create', { linkUrl, logoUrl })
 }
