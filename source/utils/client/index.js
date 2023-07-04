af103a7ec026c65887a5fb88639cb229d8ad1d79
Lewis <lewis@lewiscodes.com>
2023-07-04 11:21:47 +0100

    2604106: added ability to override services API endpoint

--- source/utils/client/index.js
+++ source/utils/client/index.js
@@ -64,8 +64,8 @@ export const servicesAPI = axios.create({
     : 'https://api.blackbaud.services'
 })
 
-const updateServicesAPIClient = () => {
-  servicesAPI.defaults.baseURL = isStaging()
+export const updateServicesAPIClient = (url) => {
+  servicesAPI.defaults.baseURL = url || isStaging()
     ? 'https://api-staging.blackbaud.services'
     : 'https://api.blackbaud.services'
 }
