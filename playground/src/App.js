8a183bbbe856b76d93d9b942bd0a2a986ce28b41
Connie Emmott-Dixon <conniedixon106@gmail.com>
2023-08-11 12:16:37 +0100

    Format with prettier

--- playground/src/App.js
+++ playground/src/App.js
@@ -1,31 +1,34 @@
-import React from 'react';
-import {
-  instance,
-  get,
-  post,
-  put,
-  destroy,
-  updateClient,
-  getBaseURL,
-  isStaging,
-  servicesAPI,
-  metadataAPI,
-  imagesAPI,
-  jgIdentityClient
-} from '../../source/utils/client'
-import InputSlug from '../../source/components/input-slug';
-import DataProvider from '../../source/utils/data';
+import React from 'react'
+// import {
+//   instance,
+//   get,
+//   post,
+//   put,
+//   destroy,
+//   updateClient,
+//   getBaseURL,
+//   isStaging,
+//   servicesAPI,
+//   metadataAPI,
+//   imagesAPI,
+//   jgIdentityClient
+// } from '../../source/utils/client'
+// import InputSlug from '../../source/components/input-slug'
+// import DataProvider from '../../source/utils/data'
 
 const App = () => {
   return (
     <>
       <h1>Supporticon Playground</h1>
-      <span>Welcome to the Supporticon playground! Add a component in App.js to get started.</span>
+      <span>
+        Welcome to the Supporticon playground! Add a component in App.js to get
+        started.
+      </span>
       {/* <DataProvider> */}
-        {/* Your component here... */}
+      {/* Your component here... */}
       {/* </DataProvider> */}
     </>
-    )
-};
+  )
+}
 
-export default App;
+export default App
