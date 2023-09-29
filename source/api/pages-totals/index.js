58468f92a38163b2f0379503917561e171460d67
Connie Emmott-Dixon <conniedixon106@gmail.com>
2023-09-29 12:21:45 +0100

    2681080 Use graphql for supporters total metric

--- source/api/pages-totals/index.js
+++ source/api/pages-totals/index.js
@@ -8,14 +8,60 @@ import {
 } from '../../utils/params'
 import { currencyCode } from '../../utils/currencies'
 import { fetchPagesByTag } from '../pages'
+import lodashGet from 'lodash/get'
+import lodashSum from 'lodash/sum'
 
 const fetchEvent = id =>
   get(`/v1/event/${id}/pages`).then(response => response.totalFundraisingPages)
 
-const fetchCampaign = id =>
-  servicesAPI
-    .get(`/v1/justgiving/campaigns/${id}/leaderboard?active=false`)
-    .then(({ data }) => data.meta.totalResults)
+
+const fetchCampaign = ({ campaign, after, limit, runningCount }) => {
+  const query = `
+  query getTotalsWithLeaderboard($id: ID, $after: String, $limit: Int!) {
+    page(type: ONE_PAGE, id: $id) {
+        leaderboard(type: FUNDRAISERS, after: $after, first: $limit) {
+          pageInfo {
+            hasNextPage
+            endCursor
+          }
+          edges {
+            node {
+              id
+            }
+          }
+        }
+      }
+    }
+  `
+
+  return servicesAPI
+    .post('/v1/justgiving/graphql', {
+      query,
+      variables: { id: campaign, after, limit }
+    })
+    .then(response => response.data)
+    .then(result => {
+      const supporterCount = lodashGet(result, 'data.page.leaderboard.edges', [])
+      const pageInfo = lodashGet(result, 'data.page.leaderboard.pageInfo', undefined)
+      return {totalCount: supporterCount?.length || 0 + runningCount, pageInfo }
+    })
+}
+
+const recursivelyFetchCampaign = ({ campaign, limit = 10, after, runningCount = 0 }) => {
+  return fetchCampaign({ campaign, limit, after, runningCount }).then(data => {
+    const { hasNextPage, endCursor } = data.pageInfo
+    if (hasNextPage) {
+      return recursivelyFetchCampaign({
+        campaign,
+        limit,
+        after: endCursor,
+        runningCount: data.totalCount
+      })
+    } else {
+      return data.totalCount
+    }
+  })
+}
 
 const fetchCampaignTeams = id =>
   servicesAPI
@@ -47,15 +93,13 @@ export const fetchPagesTotals = (params = required()) => {
 
         return Promise.all(
           campaignIds.map(getUID).map(fetchCampaignTeams)
-        ).then(campaigns => campaigns.reduce((acc, total) => acc + total, 0))
+          ).then(campaigns => campaigns.reduce((acc, total) => acc + total, 0))
       } else {
         const campaignIds = Array.isArray(params.campaign)
           ? params.campaign
           : [params.campaign]
 
-        return Promise.all(campaignIds.map(getUID).map(fetchCampaign)).then(
-          campaigns => campaigns.reduce((acc, total) => acc + total, 0)
-        )
+        return Promise.all(campaignIds.map(id => recursivelyFetchCampaign({campaign: getUID(id)}))).then(total => lodashSum(total))
       }
     default:
       return get(
