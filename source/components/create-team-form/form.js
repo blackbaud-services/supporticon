39efd8c05a3ccfb5d6b710585b1f3619760fa7fb
conniedixon <conniedixon106@gmail.com>
2024-03-14 16:02:48 +0000

    10.5.10

--- source/components/create-team-form/form.js
+++ source/components/create-team-form/form.js
@@ -4,10 +4,12 @@ export default () => ({
   fields: {
     name: {
       label: 'Team name',
-      placeholder: 'Enter your team name', 
-      validators: [validators.alphaNumericSpecial(
-        'Team name must be only letters or numbers'
-      )]
+      placeholder: 'Enter your team name',
+      validators: [
+        validators.alphaNumericSpecial(
+          'Team name must be only letters or numbers'
+        )
+      ]
     },
     target: {
       type: 'number',
